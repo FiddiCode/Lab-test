8758226d82117805a4b1756ac92d501b30d6cc14
Fardeen Qureshi <coderfardeen@gmail.com>
2024-08-24 23:50:51 +0530

    Update Card.jsx

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './card.css'
 import img from '../../assets/Medical.jpg'
-import {useNavigate} from 'react-router-dom'
+import {useNavigate } from 'react-router-dom'
 
 const Card = ({title='Demo',imgSrc='./images/demo.png',redirect='/home'}) => {
   const navigate=useNavigate();
